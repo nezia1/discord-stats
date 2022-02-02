43721884aaac34cc7af563556cfa6d1fcee660e7
Anthony Rodriguez <anthony@nezia.xyz>
2022-02-03 00:29:09 +0100

    Add parsing of servers

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <main>
+    <input type="file" accept="application/zip" @change="handleData" />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  const AdmZip = require('adm-zip')
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    methods: {
+      handleData(e) {
+        const discordData = e.target.files[0]
+        const zip = new AdmZip(discordData.path)
+        const zipEntries = zip.getEntries()
+        // Get list of servers with their ids
+        const servers = []
+        zipEntries.forEach((zipEntry) => {
+          if (!zipEntry.isDirectory) {
+            if (zipEntry.name === 'guild.json') {
+              const jsonString = zipEntry.getData().toString()
+              servers.push(JSON.parse(jsonString))
+            }
+          }
+        })
+        this.parsedData = { ...this.parsedData, servers }
+        console.log(this.parsedData)
+      },
+    },
+    data() {
+      return {
+        isExtracting: null,
+        parsedData: {},
+      }
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
