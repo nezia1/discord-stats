3b804f93c8dc4e30bededfca9e809e2f9cef5271
Anthony Rodriguez <anthony@nezia.xyz>
2022-02-03 16:45:25 +0100

    Remove how to page

--- src/router/index.js
+++ src/router/index.js
@@ -15,14 +15,6 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
-  {
-    path: '/how-to',
-    name: 'How To',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/HowTo.vue'),
-  },
 ]
 
 const router = createRouter({
