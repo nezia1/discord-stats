238524f7af840beb0d6038db66833aa169e4c4ac
Anthony Rodriguez <anthony@nezia.xyz>
2022-02-03 02:22:36 +0100

    Move the code to parse servers to its own function

--- src/App.vue
+++ src/App.vue
@@ -12,39 +12,44 @@
     methods: {
       handleData(e) {
         const discordData = e.target.files[0]
-        const zip = new AdmZip(discordData.path)
-        const zipEntries = zip.getEntries()
-        // Get message count per server and create an entry in the servers array
-        const servers = zipEntries
-          // Filters out non messages files and makes sure it belongs to a guild (not a DM conversation)
-          .filter((zipEntry) => {
-            if (zipEntry.entryName.match(/messages\/.+\/messages.csv/)) {
-              const channelId = zipEntry.entryName.split('/')[1]
-              // Gets the related channel.json file using the channel id
-              const jsonString = zip.readFile(`messages/${channelId}/channel.json`).toString()
-              const channel = JSON.parse(jsonString)
-              // If channel.json has no guild property, it's a DM / group DM conversation
-              if ('guild' in channel) {
-                return true
-              }
-              return false
-            }
-          })
-          .map((zipEntry) => {
-            const channelId = zipEntry.entryName.split('/')[1]
-            const jsonString = zip.readFile(`messages/${channelId}/channel.json`).toString()
-            const channel = JSON.parse(jsonString)
-            const serverMessageCount = zipEntry.getData().toString().split('\n').length
-            return {
-              id: channel.guild.id,
-              name: channel.guild.name,
-              messageCount: serverMessageCount,
-            }
-          })
+        const servers = this.getParsedServersData(discordData)
         const totalMessageCount = servers.reduce((total, server) => {
           return total + server.messageCount
         }, 0)
         this.parsedData = { totalMessageCount, servers }
+      },
+      getParsedServersData(discordData) {
+        const zip = new AdmZip(discordData.path)
+        const zipEntries = zip.getEntries()
+        // Get message count per server and create an entry in the servers array
+        return (
+          zipEntries
+            // Filters out non messages files and makes sure it belongs to a guild (not a DM conversation)
+            .filter((zipEntry) => {
+              if (zipEntry.entryName.match(/messages\/.+\/messages.csv/)) {
+                const channelId = zipEntry.entryName.split('/')[1]
+                // Gets the related channel.json file using the channel id
+                const jsonString = zip.readFile(`messages/${channelId}/channel.json`).toString()
+                const channel = JSON.parse(jsonString)
+                // If channel.json has no guild property, it's a DM / group DM conversation
+                if ('guild' in channel) {
+                  return true
+                }
+                return false
+              }
+            })
+            .map((zipEntry) => {
+              const channelId = zipEntry.entryName.split('/')[1]
+              const jsonString = zip.readFile(`messages/${channelId}/channel.json`).toString()
+              const channel = JSON.parse(jsonString)
+              const serverMessageCount = zipEntry.getData().toString().split('\n').length
+              return {
+                id: channel.guild.id,
+                name: channel.guild.name,
+                messageCount: serverMessageCount,
+              }
+            })
+        )
       },
     },
     data() {
