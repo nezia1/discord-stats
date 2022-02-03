fbbde3470fd2dc8a01191bcd1bf5224d0c1963fd
Anthony Rodriguez <anthony@nezia.xyz>
2022-02-03 04:31:39 +0100

    Register doughnut in main

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { Chart, registerables } from 'chart.js'
+Chart.register(...registerables)
 
+import App from './App.vue'
 createApp(App).mount('#app')
